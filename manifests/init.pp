3a0684f6c5fd5d6cbaca2a2085996618af2eb902
liamjbennett <lbennett@opentable.com>
2015-11-10 11:49:30 +0000

    Removing concat::setup as it is no longer part of the public api.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,5 @@
 class sysfs {
 
-    include concat::setup
-
     ensure_packages(["sysfsutils"])
 
     case $osfamily {
@@ -11,16 +9,16 @@ class sysfs {
           command     => "/usr/sbin/service sysfsutils restart",
           refreshonly => true,
           subscribe   => File["/etc/sysfs.conf"];
-      } 
+      }
     }
 
-    'RedHat': { 
+    'RedHat': {
         exec { 'sysfsutils_reload_rhel':
           command => '/usr/bin/awk -F= \'/(\S+)\s*=(\S+)/{cmd=sprintf("/bin/echo %s > /sys/%s",$2, $1); system(cmd)}\' /etc/sysfs.conf',
           refreshonly => true,
           subscribe => File['/etc/sysfs.conf'];
-        } 
-      } 
+        }
+      }
     }
 
     concat {
